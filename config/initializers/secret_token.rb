aaac071c97a71b09c4d63cc1abb288c8e29eb654
Ion Gonzalez <ion@suzzlo.org>
2013-01-30 22:44:39 +0100

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garage::Application.config.secret_token = '4920689db6e167370904a47d6eb2f3fbb9d9fe5a3e7d8f964758f6751f7cb6ec5a649ad95d73f52c5344a69d12d7126d64664ca48082202354e8cca707782995'
